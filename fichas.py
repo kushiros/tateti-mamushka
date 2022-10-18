066dc5366887ef8b158fa7db7766eb5ec2cc02f9
Juan Cruz Dominguez <jcdominguez01.jcd@gmail.com>
2022-10-18 12:03:47 -0300

    pequeño avance

---
+++ fichas.py
@@ -0,0 +1,12 @@
+class fichas:
+    def __init__(self, tamaño, persona):
+        self.tamaño= tamaño
+        self.persona=persona
+        self.posicion=persona.banco
+
+def colocar(self,grilla,ficha):
+    if(ficha.tamaño < self.tamaño):
+        self.posicion=ficha.posicion
+        ficha.muerta()
+def muerta(self):
+    self.posicion=self.persona.cementerio
---
+++ persona.py
@@ -0,0 +1,8 @@
+class persona:
+    def __init__(self,banco,tablero):
+        self.turno=False
+        self.cantFichas=6
+        self.banco=banco
+        self.tablero=tablero
+    def pasajeTurno(self):
+        if()
